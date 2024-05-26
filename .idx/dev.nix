015ec7b1218706a8191adce4470d940a5d745a83
mike.eling97 <mike.eling97@gmail.com>
2024-05-26 15:07:13 +0000

    first commit

---
+++ .idx/dev.nix
@@ -0,0 +1,25 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.bun
+  ];
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      # "vscodevim.vim"
+    ];
+    workspace = {
+      # Runs when a workspace is first created with this `dev.nix` file
+      onCreate = {
+        npm-install = "bun install";
+      };
+      # To run something each time the workspace is (re)started, use the `onStart` hook
+    };
+  };
+}
